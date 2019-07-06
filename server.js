cff51031d52392d5a395cbe33d3d5499527de517
Justin Mozley <jtmozley@gmail.com>
2019-07-06 15:19:41 -0500

    added process.env.port || 3000 for environment port listening

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
@@ -33,6 +33,7 @@ mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true
 });
 // Routes
+
 
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
